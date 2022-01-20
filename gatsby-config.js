184597860e7b9bfdedcfd479c7e94b4e0b69f3cd
Iqdam Musayyad <iamusayyad@gmail.com>
2022-01-20 20:39:09 +0700

    Feature: add intl package and some other markdown config

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,13 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
+        name: `home`,
+        path: `${__dirname}/src/texts/home`
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
         name: `profile`,
         path: `${__dirname}/src/texts/profile`,
       },
@@ -29,6 +36,26 @@ module.exports = {
       options: {
         name: `announcements`,
         path: `${__dirname}/src/texts/announcements`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `admissions`,
+        path: `${__dirname}/src/texts/admissions`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-intl`,
+      options: {
+        // language JSON resource path
+        path: `${__dirname}/src/intl`,
+        // supported language
+        languages: [`id`, `en`],
+        // language file path
+        defaultLanguage: `id`,
+        // option to redirect to `/ko` when connecting `/`
+        redirect: true,
       },
     },
   ],
